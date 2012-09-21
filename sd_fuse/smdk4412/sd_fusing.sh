5de1c8c1ae055e05d72d94bd073e72a8d7d363c1
eason_yen <eason_yen@coasia.com.tw>
2012-09-21 09:33:03 +0800

    [Uboot] Modify sd_fuse.sh for Ramos board.
    Description:
    1. Block unit is 512 Byte.
    2. BL1 size is 15K, block count is 30. (Block 1~30)
    3. BL2 size is 16K, block count is 32. (Block 31~62)
    4. Uboot size is 328K, block count is 656. (Block 63~718)
    5. TrustZone size is 156K, block count is 312. (Block 719~1030)
    
    Signed-off-by: eason_yen <eason_yen@coasia.com.tw>

--- sd_fuse/smdk4412/sd_fusing.sh
+++ sd_fuse/smdk4412/sd_fusing.sh
@@ -26,25 +26,25 @@ fi
 # fusing images
 
 signed_bl1_position=1
-bl2_position=17
-uboot_position=49
-tzsw_position=705
+bl2_position=31
+uboot_position=63
+tzsw_position=719
 
 #<BL1 fusing>
 echo "BL1 fusing"
-dd iflag=dsync oflag=dsync if=./E4412_D.bl1.bin of=$1 seek=$signed_bl1_position
+dd iflag=dsync oflag=dsync if=./../../E4412_N.bl1.bin of=$1 seek=$signed_bl1_position
 
 #<BL2 fusing>
 echo "BL2 fusing"
-dd iflag=dsync oflag=dsync if=./E4412_bl2.bin of=$1 seek=$bl2_position
+dd iflag=dsync oflag=dsync if=./../../bl2.bin of=$1 seek=$bl2_position
 
 #<u-boot fusing>
 echo "u-boot fusing"
-dd iflag=dsync oflag=dsync if=./u-boot.bin of=$1 seek=$uboot_position
+dd iflag=dsync oflag=dsync if=./../../u-boot.bin of=$1 seek=$uboot_position
 
 #<TrustZone S/W fusing>
 echo "TrustZone S/W fusing"
-dd iflag=dsync oflag=dsync if=./E4412_tzsw.bin of=$1 seek=$tzsw_position
+dd iflag=dsync oflag=dsync if=./../../tzsw.bin of=$1 seek=$tzsw_position
 
 ####################################
 #<Message Display>
